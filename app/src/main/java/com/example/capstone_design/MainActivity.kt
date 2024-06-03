ebc1f69ca7606d91344fcf7d887ec0f236e41e1e
tlsgptj <angele0709@naver.com>
2024-06-03 09:58:37 +0900

    git commit

--- app/src/main/java/com/example/capstone_design/MainActivity.kt
+++ app/src/main/java/com/example/capstone_design/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.capstone_design
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,18 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val firstText = findViewById<TextView>(R.id.text)
+        val SecondText = findViewById<TextView>(R.id.secondText)
+        val ThirdText = findViewById<TextView>(R.id.thirdText)
+        val tripImg = findViewById<ImageView>(R.id.Trip)
+
+
+
+
+
+
+
+
         }
     }
-}
